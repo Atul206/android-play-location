5fa7ce473ef3accbf0392a9310265a34738b4447
atul sakhala <atulsakhala@gmail.com>
2016-09-05 14:31:09 +0530

    First commit added Playstore lib

---
+++ ActivityRecognition/app/src/main/java/com/google/android/gms/location/sample/activityrecognition/DrivingBehaviourActivity.java
@@ -0,0 +1,14 @@
+package com.google.android.gms.location.sample.activityrecognition;
+
+import android.app.Application;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Atul on 05-09-2016.
+ */
+public class DrivingBehaviourActivity extends ActionBarActivity {
+
+
+
+}
